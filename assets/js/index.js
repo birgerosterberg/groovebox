6fe49627d465fe042a1f4b0595b9a631f3fbd33d
Birger <birger.oesterberg@gmail.com>
2023-05-26 16:18:25 +0200

    add reggae play/pause function

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+/**
+ * Music player for buttons onclick functionality
+ */
+// Function to play the reggae music
+
+function playReggae() {
+let reggae = document.getElementById('reggae');
+
+reggae.play();
+}
+
+// Function to pause the reggae music
+
+function pauseReggae() {
+    let reggae = document.getElementById('reggae');
+    
+    reggae.pause();
+    }
