2035f06800ca296283646613678d69ba4bee4617
Birger <birger.oesterberg@gmail.com>
2023-05-27 19:15:39 +0200

    add if check on "sample" playback, add clicked and soundplay

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,13 +4,20 @@
 // Function to play the reggae music
 function playMusic(musicId) {
     let toggleMusic = document.getElementById(musicId);
-    audio.currentTime = 0;
     return toggleMusic.paused ? toggleMusic.play() : toggleMusic.pause();
 }
 
 // Onclick audio function, takes the data-key from the audio and adds it when clicked fron the onclick function
 function clickPlay(audioIn) {
     const audio = document.querySelector(`audio[data-key="${audioIn}"]`);
+    const currentKey = document.getElementById('key-press');
+    const soundNameElement = document.getElementById('sound-played');
+    if(audio) {
+        audio.play();
+        const soundName = audio.getAttribute('data-name');
+        currentKey.innerHTML = `You clicked!`;
+        soundNameElement.innerHTML = soundName;
+    }
     audio.play();
 }
 
@@ -20,13 +27,12 @@ function keyPlay(event) {
     const currentKey = document.getElementById('key-press');
     const soundNameElement = document.getElementById('sound-played');
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    
+    // If statement to make sure its all correct and no errors is displayed.
     if(audio) {
-    audio.play();
-    const soundName = audio.getAttribute('data-name');
-    currentKey.innerHTML = event.key;
-    soundNameElement.innerHTML = soundName;
-
+        audio.play();
+        const soundName = audio.getAttribute('data-name');
+        currentKey.innerHTML = event.key;
+        soundNameElement.innerHTML = soundName;
     } else {
         currentKey.innerHTML = `Sorry ${event.key} is not used`;
         soundNameElement.innerHTML = `Sorry ${event.key} dont have a sound`;
