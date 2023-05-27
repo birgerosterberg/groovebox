be4db068d8c640f78163a121fff3e6bdbac039ac
Birger <birger.oesterberg@gmail.com>
2023-05-27 18:55:04 +0200

    add if audio statement, keypress, soundplayed

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,9 @@
  * Music player for buttons onclick functionality
  */
 // Function to play the reggae music
-function playReggae(musicId) {
+function playMusic(musicId) {
     let toggleMusic = document.getElementById(musicId);
-
+    audio.currentTime = 0;
     return toggleMusic.paused ? toggleMusic.play() : toggleMusic.pause();
 }
 
@@ -17,8 +17,19 @@ function clickPlay(audioIn) {
 
 // Keydown functions! Takes the data-keyCode info and makes sure it plays the right sound!
 function keyPlay(event) {
+    const currentKey = document.getElementById('key-press');
+    const soundNameElement = document.getElementById('sound-played');
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    console.log(event.keyCode);
+    
+    if(audio) {
     audio.play();
+    const soundName = audio.getAttribute('data-name');
+    currentKey.innerHTML = event.key;
+    soundNameElement.innerHTML = soundName;
+
+    } else {
+        currentKey.innerHTML = `Sorry ${event.key} is not used`;
+        soundNameElement.innerHTML = `Sorry ${event.key} dont have a sound`;
+    }
 }
 window.addEventListener('keydown', keyPlay);
